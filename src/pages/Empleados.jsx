dab144882775ba445af9849a4e2598c6bddc274c
Jonas Busto <jonasrex00@gmail.com>
2023-11-04 00:37:00 -0300

    cambios

--- src/pages/Empleados.jsx
+++ src/pages/Empleados.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import usuarios from "../helpers/usuarios";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-// import "../styles/empleados.css";
+import "../styles/empleados.css";
 
 const Empleados = () => {
   return (
@@ -10,18 +10,23 @@ const Empleados = () => {
       <DataTable
         className="p-datatable p-datatable-scrollable p-datatable-header p-datatable-table p-datatable-thead p-datatable-tbody p-datatable-tfoot"
         paginator
-        stripedRows
+        removableSort
+        selectionMode="single"
+        scrollHeight="400px"
         rows={5}
+        scrollable
         rowsPerPageOptions={[5, 10, 25, 50]}
         value={usuarios}
-        tableStyle={{ minWidth: "30rem" }}
       >
         <Column sortable field="id" header="ID"></Column>
         <Column sortable field="nombre" header="Nombre"></Column>
         <Column sortable field="apellido" header="Apellido"></Column>
         <Column sortable field="email" header="Email"></Column>
-        {/* <Column field="telefono" header="Telefono"></Column> */}
-        {/* <Column field="direccion" header="Dirección"></Column> */}
+        <Column field="telefono" header="Telefono"></Column>
+        <Column field="direccion" header="Dirección"></Column>
+        <Column header="Dirección">
+          <button>ce</button>
+        </Column>
       </DataTable>
     </div>
   );
